292129174c41400319815d9de46ca4cf6fbb2410
dorjibari <mdakibulislam02@gmail.com>
2022-02-15 16:35:12 +0600

    This Figma to html

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+{
+    function newBar(){
+       const x = document.getElementById('barMenu');
+       if(x.style.display === "none"){
+           x.style.display = "block";
+       }else{
+           x.style.display = "none";
+       }
+    }
+}
